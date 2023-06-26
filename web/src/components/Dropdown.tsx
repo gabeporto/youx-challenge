5c89ea5ad460f2eae3493ad348e472d779b2cba8
Gabriel Porto <7gabrielporto@gmail.com>
2023-06-26 14:50:13 -0300

    feat: add Header with dropdown

---
+++ web/src/components/Dropdown.tsx
@@ -0,0 +1,115 @@
+import { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import Logo from '../assets/youx-logo.png'
+
+const LogoImage = styled.img`
+    position: relative;
+    top: 28px;
+    left: 27px;
+    height: 59px;
+    width: 59px;
+    cursor: pointer;
+`;
+
+const DropdownContainer = styled.div`
+    position: relative;
+    display: inline-block;
+`;
+
+const DropdownContent = styled.div`
+    display: ${props => (props.itemScope ? 'block' : 'none')};
+    padding-top: 17px;
+    position: absolute;
+    top: 120px;
+    left: 0px;
+    width: 408px;
+    min-height: 100vh;
+    background-color: #023E8A;
+    border: 1px solid #707070;
+    opacity: ${props => (props.itemScope ? '1' : '0')};
+    visibility: ${props => (props.itemScope ? 'visible' : 'hidden')};
+    animation: ${props => (props.itemScope ? fadeIn : fadeOut)} 0.3s ease;
+    transition: visibility 0.3s;
+
+  &:hover {
+    visibility: visible; /* Manter visível durante o hover */
+  }
+`;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+`;
+
+const DropdownItem = styled.a`
+    display: block;
+    padding-top: 40px;
+    padding-left: 52px;
+    color: #FFFFFF;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: medium;
+    font-size: 25px;
+    opacity: 1;
+`;
+
+const DropdownSubItem = styled.a`
+    display: block;
+    margin-top: 10px;
+    padding-left: 52px;
+    color: #FFFFFF;
+    opacity: 0.5;
+    text-decoration: none;
+    font-size: 25px;
+    cursor: pointer;
+    transition: opacity 0.3s ease;
+
+    &:hover {
+        opacity: 1;
+    }
+`;
+
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <DropdownContainer>
+        <LogoImage src={Logo} alt="YouX Group Logo" onClick={toggleDropdown}/>
+        <DropdownContent itemScope={isOpen}>
+            <DropdownItem href="#">Gestão de Clientes</DropdownItem>
+            <DropdownSubItem href="#">Lista de clientes</DropdownSubItem>
+            <DropdownSubItem href="#">Cadastrar cliente</DropdownSubItem>
+
+            <DropdownItem href="#">Gestão de Vendas</DropdownItem>
+            <DropdownSubItem href="#">Lista de vendas</DropdownSubItem>
+            <DropdownSubItem href="#">Cadastrar venda</DropdownSubItem>
+
+            <DropdownItem href="#">Relatórios</DropdownItem>
+            <DropdownSubItem href="#">Relatórios</DropdownSubItem>
+        </DropdownContent>
+    </DropdownContainer>
+  );
+};
+
+export default Dropdown;
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,85 @@
+import UserIcon from '../assets/icons/UserIcon'
+import LogoutIcon from '../assets/icons/LogoutIcon'
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Dropdown from './Dropdown';
+
+const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    height: 115px;
+    background-color: #023E8A;
+`;
+
+const UserContainer = styled.div`
+    display: flex;
+    justify-items: center;
+`
+
+const UserDiv = styled.div`
+    padding-top: 25px;
+    margin-right: 30px;
+`
+
+const UserName = styled.p`
+    color: #fff;
+    font-size: 1.5rem;
+    font-weight: normal;
+`;
+
+const UserRole = styled.p`
+    color: #fff;
+    font-size: 1rem;
+    font-weight: lighter;
+    margin-top: 2px;
+`;
+
+export default function Header() {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+  
+    const hideUser = windowWidth <= 240;
+    const hideUserInformations = windowWidth <= 390;
+
+    return (
+
+        <HeaderContainer>
+            <div>
+                <Dropdown/>
+            </div>
+
+            <UserContainer>
+                {!hideUser && (
+                    <div className="py-10 mr-4">
+                        <UserIcon/>
+                    </div>
+                )}
+
+                {!hideUserInformations && (
+                    <UserDiv>
+                        <UserName>João da Silva</UserName>
+                        <UserRole>Administrador</UserRole>
+                    </UserDiv>
+                )}
+
+                <div className="py-10 mr-7">
+                    <LogoutIcon />
+                </div>
+            </UserContainer>
+        </HeaderContainer>
+
+    );
+}
