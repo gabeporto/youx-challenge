c1dbb637cceba5b27ccd0c0184c127b43c44076a
Gabriel Porto <7gabrielporto@gmail.com>
2023-06-29 09:51:49 -0300

    feat: add routes component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ClientListPage from "./pages/client/ClientListPage";
-import SaleListPage from "./pages/sale/SaleListPage";
-import ReportPage from "./pages/report/ReportPage";
+import Routes from "./components/Routes";
 
 export default function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ClientListPage />} />
-          <Route path="/client" element={<ClientListPage />} />
-          <Route path="/sale" element={<SaleListPage />} />
-          <Route path="/report" element={<ReportPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes />
   );
 }
 
---
+++ web/src/components/Routes.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ClientListPage from "../pages/client/ClientListPage";
+import SaleListPage from "../pages/sale/SaleListPage";
+import ReportPage from "../pages/report/ReportPage";
+
+export default function AllRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<ClientListPage />} />
+            <Route path="/client" element={<ClientListPage />} />
+            <Route path="/sale" element={<SaleListPage />} />
+            <Route path="/report" element={<ReportPage />} />
+            </Routes>
+      </BrowserRouter>
+    )
+}
