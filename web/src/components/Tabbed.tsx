4e222a5ebf2a6e40484298f70c2fba744b1c46e7
Gabriel Porto <7gabrielporto@gmail.com>
2023-06-30 15:39:24 -0300

    refactor: refactor tabbed component

---
+++ web/src/components/Tabbed.tsx
@@ -0,0 +1,102 @@
+import React, { ReactNode, useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    padding-left: 6%;
+`
+
+const TabContainer = styled.div`
+    width: 93%;
+    background-color: #ffffff;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const TabButtonContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    background-color: #e0e0e0;
+    border-radius: 5px 5px 0 0;
+`;
+
+const TabButton = styled.button<{ active: boolean }>`
+    width: 335px; 
+    height: 70px;
+    background-color: ${({ active }) => (active ? '#ffffff' : '#e0e0e0')};
+    border: none;
+    border-radius: 3px 3px 0 0;
+    color: #333;
+    cursor: pointer;
+    padding: 10px;
+    margin-right: 10px;
+`;
+
+const TabContent = styled.div`
+    height: 100%;
+    min-height: 565px;
+    width: 100%;
+    border: none;
+    border-radius: 0 0 3px 3p;
+    margin-bottom: 50px;
+    padding: 12px;
+`
+
+const TabTitle = styled.label`
+    text-align: left;
+    font: normal normal normal 20px/25px Helvetica Neue;
+    letter-spacing: 0px;
+    color: #5C4444;
+    opacity: 1;
+`
+
+const ContentDiv = styled.div`
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+`
+
+interface TabProps {
+    id: number,
+    title: string,
+    children: ReactNode[],
+}
+
+interface TabbedProps {
+    tabs: TabProps[]
+}
+
+export default function Tabbed(props : TabbedProps) {
+    const [activeTab, setActiveTab] = useState<number>(1);
+
+    const handleTabClick = (tab: number) => {
+    setActiveTab(tab);
+    };
+
+    return (
+        <Container>
+            <TabContainer>
+
+                <TabButtonContainer>
+                    {props.tabs.map((tab) => (
+                        <TabButton key={tab.id} active={activeTab === tab.id} onClick={() => handleTabClick(tab.id)}>
+                            <TabTitle>{tab.title}</TabTitle>
+                        </TabButton>
+                    )) }
+                </TabButtonContainer>
+
+                <TabContent>
+                    <ContentDiv>
+                        {props.tabs.map((tab) => 
+                            activeTab === tab.id && tab.children.map((children) => (
+                                children
+                            ))) 
+                        }
+                    </ContentDiv>
+                </TabContent>
+            </TabContainer>
+        </Container>
+    );
+};
+
--- web/src/pages/report/ReportPage.tsx
+++ web/src/pages/report/ReportPage.tsx
@@ -2,18 +2,12 @@ import styled from "styled-components";
 import Card from "../../components/Card";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
-import ReportTabbed from "../../components/report/ReportTabbed";
+import Tabbed from "../../components/Tabbed";
 import Container from "../../components/Container";
-
-const FlexDiv = styled.div`
-  display: flex;
-  margin-top: 42px;
-  gap: 22px;
-
-  @media only screen and (max-width: 785px) {
-    flex-wrap: wrap;
-  }
-`
+import Map from "../../components/Map";
+import InvoicingChart from '../../components/chart/InvoicingChart';
+import InvoicingTable from '../../components/report/InvoicingTable';
+import SecondaryButton from '../../components/SecondaryButton';
 
 const CardSection = styled.div`
     margin-top: 40px;
@@ -21,8 +15,153 @@ const CardSection = styled.div`
     flex-wrap: wrap;
     gap: 40px;
 `
+const InvoicingChartDiv = styled.div`
+    width: 65%;
+    min-width: 250px;
+
+    @media only screen and (max-width: 888px) {
+        width: 100%;
+        height: 100%;
+    }
+`
+
+const InvoicingTableDiv = styled.div`
+    width: 25%;
+
+    @media only screen and (max-width: 950px) {
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+    }
+`
+
+const ExportButtonDiv = styled.div`
+    width: 3%;
+
+    @media only screen and (max-width: 1460px) {
+        width: 100%;
+        height: 100%;
+    }
+`
 
 export default function ReportPage() {
+
+    // Graph
+    const monthsChart = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"];
+    const monthValuesChart = [200, 300, 100, 500, 230, 400, 230, 300, 100, 200, 300, 250];
+
+    // Table
+    const tableColumns = ["Mês", "Vendas", "Total"];
+    const dataTable = [
+        {
+            id: 1,
+            type: "report",
+            month: "Janeiro",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 2,
+            month: "Fevereiro",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 3,
+            month: "Março",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 4,
+            month: "Abril",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 5,
+            month: "Maio",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 6,
+            month: "Junho",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 7,
+            month: "Julho",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 8,
+            month: "Agosto",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 9,
+            month: "Setembro",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 10,
+            month: "Outubro",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 11,
+            month: "Novembro",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        {
+            id: 12,
+            month: "Dezembro",
+            type: "report",
+            salesQuantity: 34,
+            total: "1.245,00",
+        },
+        ];
+
+    const tabs = [
+        {
+            id: 1,
+            title: "Localização de Clientes",
+            children: [<Map height={556}/>],
+        },
+        {
+            id: 2,
+            title: "Faturamento por Mês",
+            children: [
+                <InvoicingChartDiv>
+                    <InvoicingChart columns={monthsChart} values={monthValuesChart} />
+                </InvoicingChartDiv>, 
+                <InvoicingTableDiv>
+                    <InvoicingTable columns={tableColumns} data={dataTable}/>
+                </InvoicingTableDiv>, 
+                <ExportButtonDiv>
+                    <SecondaryButton name="Exportar CSV" columns={tableColumns} data={dataTable} />
+                </ExportButtonDiv> 
+            ]
+        }
+    ]
+
     return (
         <>
             <Header />
@@ -31,15 +170,14 @@ export default function ReportPage() {
                 <Title title="Relatórios" />
 
                 <CardSection>
-                    <Card title="Vendas no Ano" value="R$ 500.000,00" iconType="money"/>
-                    <Card title="Cliente com Mais Vendas no Mês" value="LIVRARIA SEU LIVRO" iconType="graph"/>
-                    <Card title="Cliente com Maior Faturamento (Mês)" value="SEU BAR (R$ 5.000,00)" iconType="money"/>
-                    <Card title="Cliente com Maior Faturamento (Ano)" value="SEU BAR (R$ 50.000,00)" iconType="money"/>
+                    <Card key={0} title="Vendas no Ano" value="R$ 500.000,00" iconType="money"/>
+                    <Card key={1} title="Cliente com Mais Vendas no Mês" value="LIVRARIA SEU LIVRO" iconType="graph"/>
+                    <Card key={2} title="Cliente com Maior Faturamento (Mês)" value="SEU BAR (R$ 5.000,00)" iconType="money"/>
+                    <Card key={3} title="Cliente com Maior Faturamento (Ano)" value="SEU BAR (R$ 50.000,00)" iconType="money"/>
                 </CardSection>
             </Container>
-
             
-            <ReportTabbed />
+            <Tabbed tabs={tabs} />
         </>
     )
 }
