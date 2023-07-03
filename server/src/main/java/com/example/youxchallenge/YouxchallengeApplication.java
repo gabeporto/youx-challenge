39c1cb32ee61a09ab99439f74b9b7da032a6b77e
Gabriel Porto <7gabrielporto@gmail.com>
2023-07-03 15:03:07 -0300

    feat: add spring start project

---
+++ server/src/main/java/com/example/youxchallenge/YouxchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.youxchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouxchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouxchallengeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/youxchallenge/YouxchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youxchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouxchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
