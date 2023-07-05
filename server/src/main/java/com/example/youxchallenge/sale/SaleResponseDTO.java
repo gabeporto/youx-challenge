b22ae0193f6b867cce1b83774a845f0b0946738d
Gabriel Porto <7gabrielporto@gmail.com>
2023-07-05 16:35:16 -0300

    feat: change sale response to get date string

--- server/src/main/java/com/example/youxchallenge/sale/SaleResponseDTO.java
+++ server/src/main/java/com/example/youxchallenge/sale/SaleResponseDTO.java
@@ -6,12 +6,12 @@ public record SaleResponseDTO(
         Long id,
         String client,
         Long clientId,
-        Date date,
+        String date,
         String status,
         double value) {
 
     public SaleResponseDTO(Sale sale) {
-        this(sale.getId(), sale.getClient().getName(), sale.getClient().getId(), sale.getDate(), sale.getStatus(), sale.getValue());
+        this(sale.getId(), sale.getClient().getName(), sale.getClient().getId(), sale.getDate().toString(), sale.getStatus(), sale.getValue());
     }
 }
 
