7f026abe250a4e92d52472486343471b3c478007
Gabriel Porto <7gabrielporto@gmail.com>
2023-07-05 21:34:14 -0300

    feat: add date service

--- server/src/main/java/com/example/youxchallenge/service/DateService.java
+++ server/src/main/java/com/example/youxchallenge/service/DateService.java
@@ -1,2 +1,23 @@
-package com.example.youxchallenge.service;public class DateService {
+package com.example.youxchallenge.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DateService {
+
+    public List<LocalDate> getYearRange() {
+        LocalDate startDate = LocalDate.now().withDayOfYear(1);
+        LocalDate endDate = LocalDate.now().withDayOfYear(365);
+
+        List<LocalDate> localDateList = new ArrayList<>();
+        localDateList.add(startDate);
+        localDateList.add(endDate);
+
+        return localDateList;
+    }
+
 }
