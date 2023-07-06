d4d8b9f429e8d22bcefb26b1ffae49655281fdea
Gabriel Porto <7gabrielporto@gmail.com>
2023-07-05 21:33:57 -0300

    feat: add report controller

--- server/src/main/java/com/example/youxchallenge/controller/ReportController.java
+++ server/src/main/java/com/example/youxchallenge/controller/ReportController.java
@@ -1,2 +1,28 @@
-package com.example.youxchallenge.controller;public class ReportController {
+package com.example.youxchallenge.controller;
+
+import com.example.youxchallenge.service.ReportService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("report")
+public class ReportController {
+
+    @Autowired
+    ReportService reportService;
+
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public String getAllReports() {
+
+        JSONObject cards = reportService.buildCardsInformation();
+
+        return "";
+    }
+
 }
--- server/src/main/java/com/example/youxchallenge/report/ReportDTO.java
+++ server/src/main/java/com/example/youxchallenge/report/ReportDTO.java
@@ -1,2 +1,19 @@
-package com.example.youxchallenge;public record ReportDTO() {
+package com.example.youxchallenge.report;
+
+import com.example.youxchallenge.person.Person;
+import lombok.Getter;
+import lombok.Setter;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ReportDTO {
+    JSONObject cards;
+
+    JSONObject clients;
+
+    JSONObject invoicing;
+
 }
